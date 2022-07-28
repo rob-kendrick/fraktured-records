e0e56de28b5d20cdb3b24d8c5397d4545ca1930a
rob-kendrick <kendrickrobert99@gmail.com>
2022-07-28 15:02:00 +0200

    FEAT: Improved Image Carousel, added autoanimate library

--- frontend/Components/ImageCarousel/ImageCarousel.js
+++ frontend/Components/ImageCarousel/ImageCarousel.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffectw } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import styles from "../../styles/components/image-carousel/ImageCarousel.module.css";
 
 const ImageCarousel = ({ slides }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-
+  const [imageRef] = useAutoAnimate();
   //styles for individual slides
   const slideStyles = {
     width: "100%",
@@ -13,20 +14,20 @@ const ImageCarousel = ({ slides }) => {
     backgroundImage: `url(${slides[currentIndex].url})`,
   };
 
-  function goToNext() {
+  function goToPrevious() {
     const isFirstSlide = currentIndex === 0; // returns true if index = 0
-    const newIndex = isFirstSlide ? 0 : currentIndex + 1; // return 0 or +1
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1; // return 0 or +1
     setCurrentIndex(newIndex);
   }
 
-  function goToPrevious() {
+  function goToNext() {
     const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   }
 
   return (
-    <div className={styles.carousel__container}>
+    <div className={styles.carousel__container} ref={imageRef}>
       <div className={styles.leftArrowStyles} onClick={goToNext}>
         ←
       </div>
